56b97adb0a7bd473759fd648b829e7faeece8e71
Gabriel Tiso <gabrieltisovinhasdebrito2@gmail.com>
2021-12-14 14:42:55 -0300

    feat: more arch changes

--- arch-base.sh
+++ arch-base.sh
@@ -10,21 +10,29 @@ echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
 
-pacman -S grub efibootmgr
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack openssh rsync reflector acpi acpi_call tlp ipset firewalld sof-firmware nss-mdns acpid terminus-font
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Root user password: " 
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable tlp
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
+
 passwd
 
-echo "New user password: "
 useradd -m -G wheel -s /bin/bash gabriel
 passwd gabriel
 
-echo "Rebooting in 3 seconds..."
-sleep 3
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 
-exit
-reboot
