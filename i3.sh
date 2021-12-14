3c9accd7c2f018b81af82a27cb4d5827ba5b3da8
Gabriel Tiso <gabrieltisovinhasdebrito2@gmail.com>
2021-12-13 21:22:04 -0300

    feat: added i3 script

---
+++ i3.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo pacman -S --needed --noconfirm base-devel xorg-server xdotool xorg-xinit xorg-xkill xorg-xsetroot \ 
+        xwallpaper xorg-xbacklight xorg-xprop xorg-xcompmgr \ 
+        i3 alacritty zsh dhcpcd noto-fonts ttf-linux-libertine \ 
+        ttf-inconsolata libnotify dunst maim man-db xclip zsh pamixer arc-gtk-theme \
+        zathura zathura-pdf-mupdf evince
